18e1d68e72923d11b3b3c379dd59ebc7b9afa4d1
rjoshy1 <74799691+rjoshy1@users.noreply.github.com>
2022-04-03 21:06:59 -0400

    Delete express.js

--- serve_html/express.js
+++
@@ -1,8 +0,0 @@
-const path=require('path');
-const express=require('express');
-const load=express();
-load.use(express.static(__dirname+'/public'));
-load.get('/home',function(req,res) {
-  res.sendFile(path.join(__dirname+'/views/home.html'));
-});
-module.exports = load;
